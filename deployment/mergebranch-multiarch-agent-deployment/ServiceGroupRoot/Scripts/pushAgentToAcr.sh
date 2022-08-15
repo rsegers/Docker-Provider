8ea4eada45c51d578f25622aeeef988d22a7bbb4
Janvi Jatakia <jatakiajanvi12@gmail.com>
2022-08-15 10:35:11 -0700

    Making changes to the release pipeline

--- deployment/mergebranch-multiarch-agent-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
+++ deployment/mergebranch-multiarch-agent-deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
@@ -3,13 +3,13 @@ set -e
 
 # Note - This script used in the pipeline as inline script
 
-if [ -z $AGENT_IMAGE_TAG_SUFFIX ]; then
-  echo "-e error value of AGENT_IMAGE_TAG_SUFFIX variable shouldnt be empty. check release variables"
+if [ -z $AGENT_RELEASE ]; then
+  echo "-e error AGENT_RELEASE shouldnt be empty. check release variables"
   exit 1
 fi
 
-if [ -z $AGENT_RELEASE ]; then
-  echo "-e error AGENT_RELEASE shouldnt be empty. check release variables"
+if [ -z $CDPX_TAG ]; then
+  echo "-e error value of CDPX_TAG shouldn't be empty. check release variables"
   exit 1
 fi
 
@@ -22,34 +22,19 @@ fi
 
 if [[ "$AGENT_IMAGE_FULL_PATH" == *"win-"* ]]; then
   echo "checking windows tags"
-  TAG_EXISTS=$(echo $MCR_TAG_RESULT | jq '.tags | contains(["'"win-$AGENT_RELEASE$AGENT_IMAGE_TAG_SUFFIX"'"])')
+  TAG_EXISTS=$(echo $MCR_TAG_RESULT | jq '.tags | contains(["'"win-$AGENT_RELEASE$CDPX_TAG"'"])')
 else
   echo "checking linux tags"
-  TAG_EXISTS=$(echo $MCR_TAG_RESULT | jq '.tags | contains(["'"$AGENT_RELEASE$AGENT_IMAGE_TAG_SUFFIX"'"])')
+  TAG_EXISTS=$(echo $MCR_TAG_RESULT | jq '.tags | contains(["'"$AGENT_RELEASE$CDPX_TAG"'"])')
 fi
 
 if $TAG_EXISTS; then
-  echo "-e error ${AGENT_IMAGE_TAG_SUFFIX} already exists in mcr. make sure the image tag is unique"
+  echo "-e error ${CDPX_TAG} already exists in mcr. make sure the image tag is unique"
   exit 1
 fi
 
 if [ -z $AGENT_IMAGE_FULL_PATH ]; then
   echo "-e error AGENT_IMAGE_FULL_PATH shouldnt be empty. check release variables"
-  exit 1
-fi
-
-if [ -z $CDPX_TAG ]; then
-  echo "-e error value of CDPX_TAG shouldn't be empty. check release variables"
-  exit 1
-fi
-
-if [ -z $CDPX_REGISTRY ]; then
-  echo "-e error value of CDPX_REGISTRY shouldn't be empty. check release variables"
-  exit 1
-fi
-
-if [ -z $CDPX_REPO_NAME ]; then
-  echo "-e error value of CDPX_REPO_NAME shouldn't be empty. check release variables"
   exit 1
 fi
 
@@ -70,7 +55,7 @@ else
 fi     
 
 echo "Pushing ${AGENT_IMAGE_FULL_PATH} to ${ACR_NAME}"
-az acr import --name $ACR_NAME --registry $CDPX_REGISTRY --source official/${CDPX_REPO_NAME}:${CDPX_TAG} --image $AGENT_IMAGE_FULL_PATH
+az acr import --name $ACR_NAME --source mcr.microsoft.com/azuremonitor/containerinsights/cidev:${CDPX_TAG} --image $AGENT_IMAGE_FULL_PATH
 if [ $? -eq 0 ]; then
   echo "Retagged and pushed image successfully"
 else
