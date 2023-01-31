ad68bdb24426b46df05e645eaeb9fd9c6e7ad198
longwan <longwan@microsoft.com>
2023-01-31 11:00:22 -0800

    address comments

--- .pipelines/arc-register-extension-all-regions-stable.sh
+++ .pipelines/arc-register-extension-all-regions-stable.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Register azuremonitor-containers extension with Arc Registration API
+export HELM_EXPERIMENTAL_OCI=1
 
 REGISTER_REGIONS_CANARY=$REGISTER_REGIONS_CANARY
 RELEASE_TRAINS_PREVIEW_PATH=${RELEASE_TRAINS_PREVIEW_PATH}
 RELEASE_TRAINS_STABLE_PATH=${RELEASE_TRAINS_STABLE_PATH}
 IS_CUSTOMER_HIDDEN=$IS_CUSTOMER_HIDDEN
+CHART_VERSION=${CHART_VERSION}
 
 PACKAGE_CONFIG_NAME="${PACKAGE_CONFIG_NAME:-microsoft.azuremonitor.containers-pkg022022}"
 API_VERSION="${API_VERSION:-2021-05-01}"
@@ -15,7 +17,38 @@ REGISTRY_PATH_PROD_STABLE="https://mcr.microsoft.com/azuremonitor/containerinsig
 
 REGISTER_REGIONS_BATCH=($REGISTER_REGIONS_BATCH)
 
-echo "Start arc extension registration, REGISTER_REGIONS is $REGISTER_REGIONS_CANARY, RELEASE_TRAINS are $RELEASE_TRAINS_PREVIEW_PATH; $RELEASE_TRAINS_STABLE_PATH, PACKAGE_CONFIG_NAME is $PACKAGE_CONFIG_NAME, API_VERSION is $API_VERSION, METHOD is $METHOD"
+if [ -z "$REGISTER_REGIONS_CANARY" ]; then
+    echo "-e error canary release region must be provided "
+    exit 1
+fi
+if [ -z "$RELEASE_TRAINS_PREVIEW_PATH" ]; then
+    echo "-e error preview release train must be provided "
+    exit 1
+fi
+if [ -z "$RELEASE_TRAINS_STABLE_PATH" ]; then
+    echo "-e error stable release train must be provided "
+    exit 1
+fi
+if [ -z "$IS_CUSTOMER_HIDDEN" ]; then
+    echo "-e error is_customer_hidden must be provided "
+    exit 1
+fi
+if [ -z "$CHART_VERSION" ]; then
+    echo "-e error chart version must be provided "
+    exit 1
+fi
+
+MCR_NAME_PATH="mcr.microsoft.com/azuremonitor/containerinsights/prod1/stable/azuremonitor-containers"
+echo "Pulling chart from MCR:${MCR_NAME_PATH}"
+helm chart pull ${MCR_NAME_PATH}:${CHART_VERSION}
+if [ $? -eq 0 ]; then
+  echo "Pulling chart from MCR:${MCR_NAME_PATH}:${CHART_VERSION} completed successfully."
+else
+  echo "-e error Pulling chart from MCR:${MCR_NAME_PATH}:${CHART_VERSION} failed. Please review Ev2 pipeline logs for more details on the error."
+  #exit 1
+fi   
+
+echo "Start arc extension release, REGISTER_REGIONS is $REGISTER_REGIONS_CANARY, RELEASE_TRAINS are $RELEASE_TRAINS_PREVIEW_PATH; $RELEASE_TRAINS_STABLE_PATH, PACKAGE_CONFIG_NAME is $PACKAGE_CONFIG_NAME, API_VERSION is $API_VERSION, METHOD is $METHOD"
 
 # Create JSON request body
 cat <<EOF > "request.json"
@@ -82,7 +115,7 @@ cat request.json | jq
 
 # Send Request
 SUBSCRIPTION=${ADMIN_SUBSCRIPTION_ID}
-RESOURCE_ID=${RESOURCE_ID}
+RESOURCE_AUDIENCE=${RESOURCE_AUDIENCE}
 az login --service-principal --username=${SPN_CLIENT_ID} --password=${SPN_SECRET} --tenant=${SPN_TENANT_ID}
 if [ $? -eq 0 ]; then
   echo "Logged in successfully"
@@ -91,12 +124,17 @@ else
   exit 1
 fi    
 
-ACCESS_TOKEN=$(az account get-access-token --resource $RESOURCE_ID --query accessToken -o json)
+ACCESS_TOKEN=$(az account get-access-token --resource $RESOURCE_AUDIENCE --query accessToken -o json)
+if [ $? -eq 0 ]; then
+  echo "get access token from resource:$RESOURCE_AUDIENCE successfully."
+else
+  echo "-e error get access token from resource:$RESOURCE_AUDIENCE failed. Please review Ev2 pipeline logs for more details on the error."
+  exit 1
+fi  
 ACCESS_TOKEN=$(echo $ACCESS_TOKEN | tr -d '"' | tr -d '"\r\n')
 ARC_API_URL="https://eastus2euap.dp.kubernetesconfiguration.azure.com"
-VERSION=${VERSION}
 EXTENSION_NAME="microsoft.azuremonitor.containers"
-echo "Request parameter preparation, SUBSCRIPTION is $ADMIN_SUBSCRIPTION_ID, RESOURCE_ID is $RESOURCE_ID, SPN_CLIENT_ID is $SPN_CLIENT_ID, SPN_TENANT_ID is $SPN_TENANT_ID, VERSION is $VERSION"
+echo "Request parameter preparation, SUBSCRIPTION is $ADMIN_SUBSCRIPTION_ID, RESOURCE_AUDIENCE is $RESOURCE_AUDIENCE, SPN_CLIENT_ID is $SPN_CLIENT_ID, SPN_TENANT_ID is $SPN_TENANT_ID, CHART_VERSION is $CHART_VERSION"
 
 echo "start send request"
 az rest --method $METHOD --headers "{\"Authorization\": \"Bearer $ACCESS_TOKEN\", \"Content-Type\": \"application/json\"}" --body @request.json --uri $ARC_API_URL/subscriptions/$SUBSCRIPTION/extensionTypeRegistrations/$EXTENSION_NAME/versions/$VERSION?api-version=$API_VERSION
