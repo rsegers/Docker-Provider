faa340d3d1619d2328f9ae06f1f7d39f0746ff3e
Janvi Jatakia <jajataki@microsoft.com>
2023-02-10 16:59:31 -0800

    Moving to non buffered plugin

--- source/plugins/ruby/out_named_pipe.rb
+++ source/plugins/ruby/out_named_pipe.rb
@@ -46,27 +46,55 @@ module Fluent::Plugin
     end
 
     def format(tag, time, record)
-        if record != {}
-          return [tag, [[time, record]]].to_msgpack
-        else
-          return ""
-        end
+        # if record != {}
+        #   return [tag, [[time, record]]].to_msgpack
+        # else
+        #   return ""
+        # end
+        @formatter.format(tag, time, record)
     end
 
-    def write(chunk)
+    def process(tag, es)
       if @pipe_name.nil? || @pipe_name.empty?
         getNamedPipeFromExtension()
       end
       begin
-        @pipe_handle = File.open(@pipe_name, File::WRONLY)
-        chunk.write_to(@pipe_handle)
-        @pipe_handle.flush
-        @pipe_handle.close
+        if @pipe_name.nil? || @pipe_name.empty?
+          @log.info "Cannot create pipe handle skipping"
+        else
+          @pipe_handle = File.open(@pipe_name, File::WRONLY)
+          es.each do |time, record|
+            recordArray = [time, record]
+            fluentMessage = [tag]
+            fluentMessage.append([recordArray])
+            @log.info "Formatted message is: #{fluentMessage}"
+            bytes = @pipe_handle.write @formatter.format(tag, time, fluentMessage)
+            @log.info "Data bytes sent: #{bytes}"
+            @pipe_handle.flush
+          end
+          @pipe_handle.close()
+        end
       rescue Exception => e
         @log.info "Exception when writing to named pipe: #{e}"
         raise e
       end
     end
+
+
+    # def write(chunk)
+    #   if @pipe_name.nil? || @pipe_name.empty?
+    #     getNamedPipeFromExtension()
+    #   end
+    #   begin
+    #     @pipe_handle = File.open(@pipe_name, File::WRONLY)
+    #     chunk.write_to(@pipe_handle)
+    #     @pipe_handle.flush
+    #     @pipe_handle.close
+    #   rescue Exception => e
+    #     @log.info "Exception when writing to named pipe: #{e}"
+    #     raise e
+    #   end
+    # end
 
   end
 end
