c09def6ce0394751be1abbf3a3eb0f9b78b7eca2
Ganga Mahesh Siddem <gangams@microsoft.com>
2023-02-11 22:43:16 -0800

    make namedpipe output plugin thread safe

--- source/plugins/ruby/out_named_pipe.rb
+++ source/plugins/ruby/out_named_pipe.rb
@@ -11,38 +11,35 @@ module Fluent::Plugin
     def initialize
         super
         require_relative "extension_utils"
-
-        @pipe_name = ""
-        @pipe_handle = nil
-
+        # @pipe_name = ""
+        # @pipe_handle = nil
     end
 
     def configure(conf)
       super
-
       @formatter = formatter_create(usage: 'msgpack_formatter', type: 'msgpack' )
     end
 
-    def getNamedPipeFromExtension()
-        pipe_suffix = ExtensionUtils.getOutputNamedPipe(@datatype)
-        if !pipe_suffix.nil? && !pipe_suffix.empty?
-          @pipe_name = "\\\\.\\pipe\\" + pipe_suffix
-          @log.info "Named pipe: #{@pipe_name}"
-        end
-    end
+    # def getNamedPipeFromExtension()
+    #     pipe_suffix = ExtensionUtils.getOutputNamedPipe(@datatype)
+    #     if !pipe_suffix.nil? && !pipe_suffix.empty?
+    #       @pipe_name = "\\\\.\\pipe\\" + pipe_suffix
+    #       @log.info "Named pipe: #{@pipe_name}"
+    #     end
+    # end
 
     def start
       super
-      begin
-        getNamedPipeFromExtension()    
-        if @pipe_name.nil? || @pipe_name.empty?
-            @log.error "Couldn't get pipe name from extension config. Will retry during write"
-        elsif !File.exist?(@pipe_name)
-            @log.error "Named pipe with name: #{@pipe_name} doesn't exist"
-        end
-      rescue => e
-        @log.info "Exception while starting out_named_pipe #{e}"
-      end
+      # begin
+      #   getNamedPipeFromExtension()
+      #   if @pipe_name.nil? || @pipe_name.empty?
+      #       @log.error "Couldn't get pipe name from extension config. Will retry during write"
+      #   elsif !File.exist?(@pipe_name)
+      #       @log.error "Named pipe with name: #{@pipe_name} doesn't exist"
+      #   end
+      # rescue => e
+      #   @log.info "Exception while starting out_named_pipe #{e}"
+      # end
     end
 
     def format(tag, time, record)
@@ -54,14 +51,21 @@ module Fluent::Plugin
     end
 
     def write(chunk)
-      if @pipe_name.nil? || @pipe_name.empty?
-        getNamedPipeFromExtension()
-      end
+      # if @pipe_name.nil? || @pipe_name.empty?
+      #   getNamedPipeFromExtension()
+      # end
       begin
-        @pipe_handle = File.open(@pipe_name, File::WRONLY)
-        chunk.write_to(@pipe_handle)
-        @pipe_handle.flush
-        @pipe_handle.close
+        pipe_suffix = ExtensionUtils.getOutputNamedPipe(@datatype)
+        if !pipe_suffix.nil? && !pipe_suffix.empty?
+          pipe_name = "\\\\.\\pipe\\" + pipe_suffix
+          @log.info "Named pipe: #{pipe_name}"
+          pipe_handle = File.open(pipe_name, File::WRONLY)
+          chunk.write_to(pipe_handle)
+          pipe_handle.flush
+          pipe_handle.close
+        else
+          @log.error "Couldn't get pipe name from extension config. will be retried."
+        end
       rescue Exception => e
         @log.info "Exception when writing to named pipe: #{e}"
         raise e
