75064acd7f10493891cb2c6df05ecec010e3c047
Janvi Jatakia <jajataki@microsoft.com>
2023-02-15 16:56:01 -0800

    Move the default values to main.sh

--- kubernetes/linux/main.sh
+++ kubernetes/linux/main.sh
@@ -531,6 +531,8 @@ if [ -e "telemetry_prom_config_env_var" ]; then
             echo $line >>~/.bashrc
       done
       source telemetry_prom_config_env_var
+else
+      setGlobalEnvVar TELEMETRY_CUSTOM_PROM_MONITOR_PODS false
 fi
 
 #Parse sidecar agent settings for custom configuration
@@ -576,13 +578,15 @@ if [[ ( ( ! -e "/etc/config/kube.conf" ) && ( "${CONTAINER_TYPE}" == "Prometheus
                   echo $line >>~/.bashrc
             done
             source integration_osm_config_env_var
+      else
+            setGlobalEnvVar TELEMETRY_OSM_CONFIGURATION_NAMESPACES_COUNT 0
       fi
 fi
 
 # If the prometheus sidecar isn't doing anything then there's no need to run mdsd and telegraf in it.
 if [[ ( "${CONTAINER_TYPE}" == "PrometheusSidecar" ) &&
-      ( "${TELEMETRY_CUSTOM_PROM_MONITOR_PODS}" == "false"  || "${TELEMETRY_CUSTOM_PROM_MONITOR_PODS}" == "" ) &&
-      ( "${TELEMETRY_OSM_CONFIGURATION_NAMESPACES_COUNT}" -eq 0 || "${TELEMETRY_OSM_CONFIGURATION_NAMESPACES_COUNT}" == "" ) ]]; then
+      ( "${TELEMETRY_CUSTOM_PROM_MONITOR_PODS}" == "false" ) &&
+      ( "${TELEMETRY_OSM_CONFIGURATION_NAMESPACES_COUNT}" -eq 0 ) ]]; then
       setGlobalEnvVar MUTE_PROM_SIDECAR true
 else
       setGlobalEnvVar MUTE_PROM_SIDECAR false
