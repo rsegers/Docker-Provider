3b8ec51274d35f1bc88790e31c0bac0cf6d75209
Ganga Mahesh Siddem <gangams@microsoft.com>
2023-02-20 21:12:37 -0800

    add mutex to synchorize namedpipe writes

--- source/plugins/ruby/out_named_pipe.rb
+++ source/plugins/ruby/out_named_pipe.rb
@@ -1,23 +1,24 @@
-require 'fluent/plugin/output'
+require "fluent/plugin/output"
 
 module Fluent::Plugin
   class NamedPipeOutput < Output
-    Fluent::Plugin.register_output('named_pipe', self)
+    Fluent::Plugin.register_output("named_pipe", self)
 
     helpers :formatter
 
     config_param :datatype, :string
 
     def initialize
-        super
-        require_relative "extension_utils"
-        # @pipe_name = ""
-        # @pipe_handle = nil
+      super
+      require_relative "extension_utils"
+      @semaphore = Mutex.new
+      # @pipe_name = ""
+      # @pipe_handle = nil
     end
 
     def configure(conf)
       super
-      @formatter = formatter_create(usage: 'msgpack_formatter', type: 'msgpack' )
+      @formatter = formatter_create(usage: "msgpack_formatter", type: "msgpack")
     end
 
     # def getNamedPipeFromExtension()
@@ -43,11 +44,11 @@ module Fluent::Plugin
     end
 
     def format(tag, time, record)
-        if record != {}
-          return [tag, [[time, record]]].to_msgpack
-        else
-          return ""
-        end
+      if record != {}
+        return [tag, [[time, record]]].to_msgpack
+      else
+        return ""
+      end
     end
 
     def write(chunk)
@@ -55,22 +56,23 @@ module Fluent::Plugin
       #   getNamedPipeFromExtension()
       # end
       begin
-        pipe_suffix = ExtensionUtils.getOutputNamedPipe(@datatype)
-        if !pipe_suffix.nil? && !pipe_suffix.empty?
-          pipe_name = "\\\\.\\pipe\\" + pipe_suffix
-          @log.info "Named pipe: #{pipe_name}"
-          pipe_handle = File.open(pipe_name, File::WRONLY)
-          chunk.write_to(pipe_handle)
-          pipe_handle.flush
-          pipe_handle.close
-        else
-          @log.error "Couldn't get pipe name from extension config. will be retried."
-        end
+        @semaphore.synchronize {
+          pipe_suffix = ExtensionUtils.getOutputNamedPipe(@datatype)
+          if !pipe_suffix.nil? && !pipe_suffix.empty?
+            pipe_name = "\\\\.\\pipe\\" + pipe_suffix
+            @log.info "Named pipe: #{pipe_name}"
+            pipe_handle = File.open(pipe_name, File::WRONLY)
+            chunk.write_to(pipe_handle)
+            pipe_handle.flush
+            pipe_handle.close
+          else
+            @log.error "Couldn't get pipe name from extension config. will be retried."
+          end
+        }
       rescue Exception => e
         @log.info "Exception when writing to named pipe: #{e}"
         raise e
       end
     end
-
   end
 end
