fec93c985cf5336b7b273b8345149213e0cf098d
Amol Agrawal <amagraw@microsoft.com>
2023-02-26 19:15:24 +0000

    enable traces

--- kubernetes/linux/main.sh
+++ kubernetes/linux/main.sh
@@ -779,7 +779,7 @@ else
       echo "export MDSD_ROLE_PREFIX=$MDSD_ROLE_PREFIX" >> ~/.bashrc
       source ~/.bashrc
       mkdir /var/run/mdsd-ci
-      mdsd ${MDSD_AAD_MSI_AUTH_ARGS} -r ${MDSD_ROLE_PREFIX} -e ${MDSD_LOG}/mdsd.err -w ${MDSD_LOG}/mdsd.warn -o ${MDSD_LOG}/mdsd.info -q ${MDSD_LOG}/mdsd.qos 2>>/dev/null &
+      mdsd ${MDSD_AAD_MSI_AUTH_ARGS} -T 0xFFFF -r ${MDSD_ROLE_PREFIX} -e ${MDSD_LOG}/mdsd.err -w ${MDSD_LOG}/mdsd.warn -o ${MDSD_LOG}/mdsd.info -q ${MDSD_LOG}/mdsd.qos 2>>/dev/null &
 fi
 
 # Set up a cron job for logrotation
