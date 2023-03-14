4b5e108fe2dcadc2043d65702b9e01361ed92f28
Janvi Jatakia <jajataki@microsoft.com>
2023-03-14 12:06:45 -0700

    Add crontab

--- kubernetes/linux/main.sh
+++ kubernetes/linux/main.sh
@@ -784,11 +784,11 @@ else
       mdsd ${MDSD_AAD_MSI_AUTH_ARGS} -r ${MDSD_ROLE_PREFIX} -e ${MDSD_LOG}/mdsd.err -w ${MDSD_LOG}/mdsd.warn -o ${MDSD_LOG}/mdsd.info -q ${MDSD_LOG}/mdsd.qos 2>>/dev/null &
 fi
 
-# Set up a cron job for logrotation
-if [ ! -f /etc/cron.d/ci-agent ]; then
-      echo "setting up cronjob for ci agent log rotation"
-      echo "*/5 * * * * root /usr/sbin/logrotate -s /var/lib/logrotate/ci-agent-status /etc/logrotate.d/ci-agent >/dev/null 2>&1" >/etc/cron.d/ci-agent
-fi
+# # Set up a cron job for logrotation
+# if [ ! -f /etc/cron.d/ci-agent ]; then
+#       echo "setting up cronjob for ci agent log rotation"
+#       echo "*/5 * * * * root /usr/sbin/logrotate -s /var/lib/logrotate/ci-agent-status /etc/logrotate.d/ci-agent >/dev/null 2>&1" >/etc/cron.d/ci-agent
+# fi
 
 # no dependency on fluentd for prometheus side car container
 if [ "${CONTAINER_TYPE}" != "PrometheusSidecar" ]; then
