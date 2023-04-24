8963454f167e5e644bb451a462b11c4d677d4b3b
Janvi Jatakia <jajataki@microsoft.com>
2023-04-24 13:37:53 -0700

    Only using gem install

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -12,16 +12,16 @@ fi
 sudo tdnf install ca-certificates-microsoft -y
 sudo update-ca-trust
 
-# sudo tdnf install ruby-3.1.3 -y
-tdnf install -y gcc patch bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
-wget https://github.com/rbenv/ruby-build/archive/refs/tags/v20230330.tar.gz -O ruby-build.tar.gz
-tar -xzf ruby-build.tar.gz
-PREFIX=/usr/local ./ruby-build-*/install.sh
-ruby-build 3.1.3 /usr
+sudo tdnf install ruby-3.1.3 -y
+# tdnf install -y gcc patch bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
+# wget https://github.com/rbenv/ruby-build/archive/refs/tags/v20230330.tar.gz -O ruby-build.tar.gz
+# tar -xzf ruby-build.tar.gz
+# PREFIX=/usr/local ./ruby-build-*/install.sh
+# ruby-build 3.1.3 /usr
 
-# clean up the ruby-build files
-rm ruby-build.tar.gz
-rm -rf ruby-build-*
+# # clean up the ruby-build files
+# rm ruby-build.tar.gz
+# rm -rf ruby-build-*
 
 # remove unused default gem openssl, find as they have some known vulns
 rm /usr/lib/ruby/gems/3.1.0/specifications/default/openssl-3.0.1.gemspec
