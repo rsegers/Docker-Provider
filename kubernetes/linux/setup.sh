764b872dde710748a51247a5f7f5ea50165f84d2
Janvi Jatakia <jajataki@microsoft.com>
2023-04-24 08:29:26 -0700

    Testing ruby using rvm install

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -12,12 +12,31 @@ fi
 sudo tdnf install ca-certificates-microsoft -y
 sudo update-ca-trust
 
-sudo tdnf install ruby-3.1.3 -y
-# remove unused default gem openssl, find as they have some known vulns
-rm /usr/lib/ruby/gems/3.1.0/specifications/default/openssl-3.0.1.gemspec
-rm -rf /usr/lib/ruby/gems/3.1.0/gems/openssl-3.0.1
-rm /usr/lib/ruby/gems/3.1.0/specifications/default/find-0.1.1.gemspec
-rm -rf /usr/lib/ruby/gems/3.1.0/gems/find-0.1.1
+# sudo tdnf install ruby-3.1.3 -y
+# # remove unused default gem openssl, find as they have some known vulns
+# rm /usr/lib/ruby/gems/3.1.0/specifications/default/openssl-3.0.1.gemspec
+# rm -rf /usr/lib/ruby/gems/3.1.0/gems/openssl-3.0.1
+# rm /usr/lib/ruby/gems/3.1.0/specifications/default/find-0.1.1.gemspec
+# rm -rf /usr/lib/ruby/gems/3.1.0/gems/find-0.1.1
+
+tdnf install which procps-ng -y
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s stable
+
+# setup paths for ruby and rvm
+if [ -f /etc/profile.d/rvm.sh ]; then
+    source /etc/profile.d/rvm.sh
+    echo "[ -f /etc/profile.d/rvm.sh ] && source /etc/profile.d/rvm.sh" >> ~/.bashrc
+fi
+
+rvm install 3.1.3
+rvm --default use 3.1.3
+# remove unused default gem openssl, find and rvm as they have some known vulns
+rm /usr/local/rvm/rubies/ruby-3.1.3/lib/ruby/gems/3.1.0/specifications/default/openssl-3.0.1.gemspec
+rm /usr/local/rvm/rubies/ruby-3.1.3/lib/ruby/gems/3.1.0/specifications/default/find-0.1.1.gemspec
+rm /usr/local/rvm/rubies/ruby-3.1.3/lib/ruby/gems/3.1.0/specifications/rvm-1.11.3.9.gemspec
+
 
 if [ "${ARCH}" != "arm64" ]; then
     wget "https://github.com/microsoft/Docker-Provider/releases/download/official%2Fmdsd%2F1.17.1%2Frpm/azure-mdsd_1.17.1-build.master.377_x86_64.rpm" -O azure-mdsd.rpm
@@ -62,7 +81,8 @@ sudo tdnf install fluent-bit-2.0.9 -y
 echo "$(fluent-bit --version)" >> packages_version.txt
 
 # install fluentd using the mariner package
-sudo tdnf install rubygem-fluentd-1.14.6 -y
+# sudo tdnf install rubygem-fluentd-1.14.6 -y
+gem install fluentd -v "1.14.6" --no-document
 echo "$(fluentd --version)" >> packages_version.txt
 fluentd --setup ./fluent
 
