7b29c40aaf418e4db3f1b9d55faf7e98f0719e45
Ganga Mahesh Siddem <gangams@microsoft.com>
2023-07-04 13:23:31 -0700

    cleanup script for log app

---
+++ test/containerlog-scale-tests/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## each job generates 1 kps rate,
+
+## define N value, 10K eps (each log line size 1KB), N value be 10 and similarly if its 50K eps, N value must be 50
+N=10
+
+for ((i=1; i <= N; i++))
+do
+   kubectl delete ns test"$i"
+done
+
