9d28d8b8414047c2ce5dfd7921edd42978a8e7c9
Ganga Mahesh Siddem <gangams@microsoft.com>
2023-07-24 10:25:36 -0700

    fix typos

--- test/containerlog-scale-tests/cleanup.sh
+++ test/containerlog-scale-tests/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-## each job generates 1 kps rate,
+## This is to clean up the jobs created by deploy script
+## define N value to match with deploy script to clean up the resources created by deploy script
 
-## define N value, 10K eps (each log line size 1KB), N value be 10 and similarly if its 50K eps, N value must be 50
 N=5
 
 for ((i=1; i <= N; i++))
--- test/containerlog-scale-tests/deploy.sh
+++ test/containerlog-scale-tests/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-## each job generates 1 kps rate,
-
-## define N value, 10K eps (each log line size 1KB), N value be 10 and similarly if its 50K eps, N value must be 50
+## each job generates  1K logs/sec rate
+## define N value, 10K logs/sec (each log line size 1KB), N value be 10 and similarly if its 50K logs/sec, N value must be 50
 N=5
 
 for ((i=1; i <= N; i++))
