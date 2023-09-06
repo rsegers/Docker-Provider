b1cdc34a601d8aeb2c1cdda07300e0d0123aa9c1
Alexander Kaplan <alkaplan@microsoft.com>
2023-09-06 13:42:23 -0700

    Updating NodeJs SDK version (#1061)

--- appmonitoring/ts/src/AddedTypes.ts
+++ appmonitoring/ts/src/AddedTypes.ts
@@ -9,7 +9,7 @@ export class AddedTypes {
     // agent image
     private static agentImageDotNet = "mcr.microsoft.com/applicationinsights/opentelemetry-auto-instrumentation/dotnet:1.0.0-beta2";
     private static agentImageJava = "mcr.microsoft.com/applicationinsights/auto-instrumentation/java:3.4.15";
-    private static agentImageNodeJs = "mcr.microsoft.com/applicationinsights/opentelemetry-auto-instrumentation/nodejs:3.0.0-beta.8";
+    private static agentImageNodeJs = "mcr.microsoft.com/applicationinsights/opentelemetry-auto-instrumentation/nodejs:3.0.0-beta.9";
     
     // path on agent image to copy from
     private static imagePathDotNet = "/dotnet-tracer-home/.";
