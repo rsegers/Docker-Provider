737408aeea6c15ac0fbb4e3df689252daf89259d
longwan <longwan@microsoft.com>
2023-09-27 19:47:18 -0700

    update comment

--- source/plugins/go/src/telemetry.go
+++ source/plugins/go/src/telemetry.go
@@ -230,7 +230,7 @@ func SendContainerLogPluginMetrics(telemetryPushIntervalProperty string) {
 					telemetryDimensions["mdsdCompressionLevel"] = mdsdCompressionLevel
 				}
 
-				// TODO verify this also fetched from machine level
+				// TODO verify this also fetched from machine level 
 				telemetryDimensions["PromFbitChunkSize"] = os.Getenv("AZMON_FBIT_CHUNK_SIZE")
 				telemetryDimensions["PromFbitBufferSize"] = os.Getenv("AZMON_FBIT_BUFFER_SIZE")
 				telemetryDimensions["PromFbitMemBufLimit"] = os.Getenv("AZMON_FBIT_MEM_BUF_LIMIT")
