d06c401fa01cfa5edebee58d3f1e0043b60ea35d
Ramanuj Bhattacharjee (from Dev Box) <rambhatt@microsoft.com>
2023-12-15 19:10:44 -0800

    add changes

--- appmonitoring/scripts/validate_ai.sh
+++ appmonitoring/scripts/validate_ai.sh
@@ -11,13 +11,20 @@ tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47"
 client_id="02a35d9f-68ea-4a99-b688-c3a359d7cab0"
 client_secret="__raw__"
 
-# Make the POST request
-response=$(curl -s -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "grant_type=client_credentials&client_id=$client_id&resource=https://api.applicationinsights.io&client_secret=$client_secret")
+# Get an access token
+access_token=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/' -H Metadata:true -s | jq -r '.access_token')
 
-# Parse the access_token from the response
-access_token=$(echo $response | jq -r '.access_token')
+# Use the access token
+# curl 'https://management.azure.com/subscriptions/<subscription-id>/resourceGroups/<resource-group>/providers/Microsoft.Compute/virtualMachines/<vm-name>?api-version=2019-03-01' -H Metadata:true -H "Authorization: Bearer $access_token"
+
+
+# Make the POST request
+# response=$(curl -s -X POST https://login.microsoftonline.com/$tenant_id/oauth2/token \
+#   -H "Content-Type: application/x-www-form-urlencoded" \
+#   -d "grant_type=client_credentials&client_id=$client_id&resource=https://api.applicationinsights.io&client_secret=$client_secret")
+
+# # Parse the access_token from the response
+# access_token=$(echo $response | jq -r '.access_token')
 
 # Print the access_token
 echo "Access Token: $access_token"
