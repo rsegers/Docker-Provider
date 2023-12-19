0f21bac0cff25455fc31104a8e398469bf81bcc7
Ramanuj Bhattacharjee (from Dev Box) <rambhatt@microsoft.com>
2023-12-18 17:09:33 -0800

    hide outputs

--- appmonitoring/scripts/validate_ai.sh
+++ appmonitoring/scripts/validate_ai.sh
@@ -9,7 +9,7 @@
 
 # Get an access token
 result_rsp=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://api.applicationinsights.io&mi_res_id=/subscriptions/66010356-d8a5-42d3-8593-6aaa3aeb1c11/resourceGroups/rambhatt-rnd-v2/providers/Microsoft.ManagedIdentity/userAssignedIdentities/rambhatt-agentpool-es-identity' -H Metadata:true -s)
-echo "Result: $result_rsp"
+# echo "Result: $result_rsp"
 access_token=$(echo $result_rsp | jq -r '.access_token')
 
 # Print the access_token
@@ -40,10 +40,10 @@ response=$(curl -s -X POST $url \
   -d "$json_body")
 
 # Print the response
-echo "Response: $response"
+# echo "Response: $response"
 
 count_val=$(echo $response | jq '.tables[0].rows[0][0]')
-echo "Count: $count_val"
+# echo "Count: $count_val"
 
 if (( count_val > 0 )); then
     echo $count_val
