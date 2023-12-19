b0dbbaf64a054e1a04b07b8347b5e4ba3e616163
Ramanuj Bhattacharjee (from Dev Box) <rambhatt@microsoft.com>
2023-12-18 18:39:04 -0800

    hold these changes

--- appmonitoring/scripts/validate_ai.sh
+++ appmonitoring/scripts/validate_ai.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# POD_DOTNET_NAME=$1
-# POD_JAVA_NAME=$2
-# POD_NODEJS_NAME=$3
-# AI_RES_ID=$4
-# NAMESPACE=$5
-
+POD_DOTNET_NAME=$1
+POD_JAVA_NAME=$2
+POD_NODEJS_NAME=$3
+AI_RES_ID=$4
 
 # Get an access token
 result_rsp=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://api.applicationinsights.io&mi_res_id=/subscriptions/66010356-d8a5-42d3-8593-6aaa3aeb1c11/resourceGroups/rambhatt-rnd-v2/providers/Microsoft.ManagedIdentity/userAssignedIdentities/rambhatt-agentpool-es-identity' -H Metadata:true -s)
@@ -13,36 +11,44 @@ result_rsp=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-ver
 access_token=$(echo $result_rsp | jq -r '.access_token')
 
 # Define your variables
-url="https://api.loganalytics.io/v1/subscriptions/66010356-d8a5-42d3-8593-6aaa3aeb1c11/resourceGroups/rambhatt-rnd-v2/providers/microsoft.insights/components/aks-final-2/query"
+url="https://api.loganalytics.io/v1$AI_RES_ID/query"
 
-# Define the JSON body
+verify_AI_telemetry() {
+    echo $1
+    json_body="{
+        'query': 'traces | where timestamp > ago(5m) | where cloud_RoleName == \"$1\" | count',
+        'options': {
+            'truncationMaxSize': 67108864
+        },
+        'maxRows': 30001,
+        'workspaceFilters': {
+            "regions": []
+        }
+    }";
 
-json_body='{
-    "query": "traces | where timestamp > ago(5m) | where cloud_RoleName == \"aks-demo-app\" | count",
-    "options": {
-        "truncationMaxSize": 67108864
-    },
-    "maxRows": 30001,
-    "workspaceFilters": {
-        "regions": []
-    }
-}'
+    echo $json_body
 
-# Make the POST request
-response=$(curl -s -X POST $url \
-  -H "Authorization: Bearer $access_token" \
-  -H "Content-Type: application/json" \
-  -d "$json_body")
+    # Make the POST request
+    response=$(curl -s -X POST $url \
+    -H "Authorization: Bearer $access_token" \
+    -H "Content-Type: application/json" \
+    -d "$json_body")
 
-# Print the response
-# echo "Response: $response"
+    # Print the response
+    # echo "Response: $response"
 
-count_val=$(echo $response | jq '.tables[0].rows[0][0]')
-# echo "Count: $count_val"
+    count_val=$(echo $response | jq '.tables[0].rows[0][0]')
+    # echo "Count: $count_val"
 
-if (( count_val > 0 )); then
-    echo $count_val
-else
-    echo "Not found any appropriate records" >&2
-    exit 1
-fi
+    if (( count_val > 0 )); then
+        echo $count_val
+    else
+        echo "Not found any appropriate records" >&2
+        exit 1
+    fi
+}
+
+verify_AI_telemetry "$POD_DOTNET_NAME"
+verify_AI_telemetry "$POD_JAVA_NAME"
+verify_AI_telemetry "$POD_NODEJS_NAME"
+
