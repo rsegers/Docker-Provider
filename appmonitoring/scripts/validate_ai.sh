d7caf24b431a806ed7baa46d97f895b92ba5e66f
Ramanuj Bhattacharjee <103154538+ramanujbhattacharjee93@users.noreply.github.com>
2024-01-26 16:32:18 -0800

    Finish up validation pipeline (#1143)
    * initial commit
    
    * add changes
    
    * Update azure_pipeline_validation_appmonitoring.yaml for Azure Pipelines
    
    * add fix
    
    * add changes
    
    * add tenant reference
    
    * see the message
    
    * try with Mi res id
    
    * commit update
    
    * re add steps
    
    * hide outputs
    
    * remove ARM stuff (for now)
    
    * remove token exposure
    
    * hold these changes
    
    * see this change
    
    * Update azure_pipeline_validation_appmonitoring.yaml for Azure Pipelines
    
    * Update azure_pipeline_validation_appmonitoring.yaml for Azure Pipelines
    
    * add path fix
    
    * fix script issue
    
    * fix resource linkages
    
    * try reuploading image
    
    * add changes
    
    * add changes
    
    * Fix app monitoring pipeline script
    
    * add changes
    
    * fix image build specificity
    
    * try fix variable
    
    * try this now
    
    * add changes for PR
    
    * remove stale comments
    
    * add codeQL
    
    * add fixed change
    
    * fix deletion script
    
    * wait longer to telemetry to appear
    
    * add changes
    
    ---------
    
    Co-authored-by: Ramanuj Bhattacharjee (from Dev Box) <rambhatt@microsoft.com>

---
+++ appmonitoring/scripts/validate_ai.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+POD_DOTNET_NAME=$1
+POD_JAVA_NAME=$2
+POD_NODEJS_NAME=$3
+AI_RES_ID=$4
+
+# Get an access token
+result_rsp=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://api.applicationinsights.io&mi_res_id=/subscriptions/66010356-d8a5-42d3-8593-6aaa3aeb1c11/resourceGroups/rambhatt-rnd-v2/providers/Microsoft.ManagedIdentity/userAssignedIdentities/rambhatt-agentpool-es-identity' -H Metadata:true -s)
+# echo "Result: $result_rsp"
+access_token=$(echo $result_rsp | jq -r '.access_token')
+
+echo $AI_RES_ID
+
+# Define your variables
+url="https://api.loganalytics.io/v1$AI_RES_ID/query"
+
+verify_AI_telemetry() {
+    echo $1
+    json_body="{
+        'query': 'traces | where timestamp > ago(15m) | where cloud_RoleName == \"$1\" | count',
+        'options': {
+            'truncationMaxSize': 67108864
+        },
+        'maxRows': 30001,
+        'workspaceFilters': {
+            "regions": []
+        }
+    }";
+
+    echo $json_body
+
+    # Make the POST request
+    response=$(curl -s -X POST $url \
+    -H "Authorization: Bearer $access_token" \
+    -H "Content-Type: application/json" \
+    -d "$json_body")
+
+
+    count_val=$(echo $response | jq '.tables[0].rows[0][0]')
+
+    if (( count_val > 0 )); then
+        echo $count_val
+    else
+        echo "Not found any appropriate records" >&2
+        echo "Validation for $2 pods failed" >&2
+        exit 1
+    fi
+}
+
+verify_AI_telemetry "$POD_DOTNET_NAME" "dotnet"
+verify_AI_telemetry "$POD_JAVA_NAME" "java"
+verify_AI_telemetry "$POD_NODEJS_NAME" "nodejs"
+
