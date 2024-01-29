c1a973ae56795c3a5065fa7282ef315649d537ac
Janvi Jatakia (from Dev Box) <jajataki@microsoft.com>
2024-01-29 11:44:37 -0800

    Solving timestamp bug and add windows_rs.conf files

--- kubernetes/linux/main.sh
+++ kubernetes/linux/main.sh
@@ -933,7 +933,7 @@ if [ ! -f /etc/cron.d/ci-agent ]; then
       echo "*/5 * * * * root /usr/sbin/logrotate -s /var/lib/logrotate/ci-agent-status /etc/logrotate.d/ci-agent >/dev/null 2>&1" >/etc/cron.d/ci-agent
 fi
 
-if [ "${USING_AAD_MSI_AUTH}" != "true" ]; then
+if [ "${USING_AAD_MSI_AUTH}" != "true" ] || [ "${GENEVA_LOGS_INTEGRATION}" == "true" ]; then
       if [ -e "/etc/config/kube.conf" ]; then
            # Replace a string in the configmap file
             sed -i "s/#@include windows_rs.conf/@include windows_rs.conf/g" /etc/fluent/kube.conf
--- source/plugins/go/input/lib/cadvisor.go
+++ source/plugins/go/input/lib/cadvisor.go
@@ -781,22 +781,29 @@ func getNodeLastRebootTimeMetric(metricInfo map[string]interface{}, hostName, me
 		return nodeMetricItem
 	}
 
-	var timeDifference int64
+	var epochTime int64
 	if osType != "" && strings.EqualFold(osType, "windows") && IsAADMSIAuthMode() {
 		//Read from "C:\\etc\\kubernetes\\host\\windowsnodereset.log"
-		uptimeStr, err := ioutil.ReadFile("C:\\etc\\kubernetes\\host\\windowsnodereset.log")
+		startTimeStr, err := ioutil.ReadFile("C:\\etc\\kubernetes\\host\\windowsnodereset.log")
 		if err != nil {
 			Log.Warnf("Error reading C:\\etc\\kubernetes\\host\\windowsnodereset.log: %s", err)
 			return nodeMetricItem
 		}
-		uptimeDateTimeStr := strings.Fields(string(uptimeStr))[0]
-		//convert the string to a time and then to a float
-		uptimeDateTime, err := time.Parse(time.RFC3339, uptimeDateTimeStr)
+		//parse the string with the layout to get the time
+		startDateTimeStr := strings.Fields(string(startTimeStr))[0]
+		layout := "2006-01-02T15:04:05.9999999-07:00:"
+		startDateTime, err := time.Parse(layout, startDateTimeStr)
+		if err != nil {
+			Log.Warnf("Error parsing time according to layout: %s", err)
+			return nodeMetricItem
+		}
+		formattedDateTime := startDateTime.UTC().Format(time.RFC3339)
+		startTime, err := time.Parse(time.RFC3339, formattedDateTime)
 		if err != nil {
 			Log.Warnf("Error parsing time: %s", err)
 			return nodeMetricItem
 		}
-		timeDifference = parsedTime.Unix() - uptimeDateTime.Unix()
+		epochTime = startTime.Unix()
 	} else {
 		// Read the first value from /proc/uptime and convert it to a float64
 		uptimeStr, err := ioutil.ReadFile("/proc/uptime")
@@ -811,7 +818,7 @@ func getNodeLastRebootTimeMetric(metricInfo map[string]interface{}, hostName, me
 			return nodeMetricItem
 		}
 
-		timeDifference = parsedTime.Unix() - int64(uptimeSeconds)
+		epochTime = parsedTime.Unix() - int64(uptimeSeconds)
 	}
 
 	metricCollection := map[string]interface{}{
