6b708fc541565bad4558dd51cb4000ff31fbf969
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-02-19 20:48:06 -0800

    bug fix

--- source/plugins/go/src/extension/extension_linux.go
+++ source/plugins/go/src/extension/extension_linux.go
@@ -4,8 +4,10 @@ package extension
 
 import (
 	"fmt"
-	"github.com/ugorji/go/codec"
+	"os"
 	"strings"
+
+	"github.com/ugorji/go/codec"
 )
 
 func getExtensionConfigResponse(jsonBytes []byte) ([]byte, error) {
@@ -17,8 +19,11 @@ func getExtensionConfigResponse(jsonBytes []byte) ([]byte, error) {
 
 	fs := &FluentSocket{}
 	fs.sockAddress = "/var/run/mdsd-ci/default_fluent.socket"
+	genevaLogsIntegrationEnabled := strings.TrimSpace(strings.ToLower(os.Getenv("GENEVA_LOGS_INTEGRATION")))
 	if containerType != "" && strings.Compare(strings.ToLower(containerType), "prometheussidecar") == 0 {
 		fs.sockAddress = fmt.Sprintf("/var/run/mdsd-%s/default_fluent.socket", containerType)
+	} else if genevaLogsIntegrationEnabled != "" && strings.Compare(strings.ToLower(genevaLogsIntegrationEnabled), "true") == 0 {
+		fs.sockAddress = "/var/run/mdsd-PrometheusSidecar/default_fluent.socket"
 	}
 	responseBytes, err := FluentSocketWriter.writeAndRead(fs, data)
 	defer FluentSocketWriter.disconnect(fs)
