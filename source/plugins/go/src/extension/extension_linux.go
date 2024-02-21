7de4e888b2662553db3932d47eb87adbc0fb5996
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-02-21 10:40:20 -0800

    fix pr feedback

--- source/plugins/go/src/extension/extension_linux.go
+++ source/plugins/go/src/extension/extension_linux.go
@@ -3,12 +3,14 @@
 package extension
 
 import (
-	"fmt"
 	"os"
 	"strings"
 
 	"github.com/ugorji/go/codec"
 )
+
+const FluentSocketAddress = "/var/run/mdsd-ci/default_fluent.socket"
+const FluentSocketAddressPrometheysSidecar = "/var/run/mdsd-PrometheusSidecar/default_fluent.socket"
 
 func getExtensionConfigResponse(jsonBytes []byte) ([]byte, error) {
 	var data []byte
@@ -18,21 +20,17 @@ func getExtensionConfigResponse(jsonBytes []byte) ([]byte, error) {
 	}
 
 	fs := &FluentSocket{}
-	fs.sockAddress = "/var/run/mdsd-ci/default_fluent.socket"
+	fs.sockAddress = FluentSocketAddress
 	genevaLogsIntegrationEnabled := strings.TrimSpace(strings.ToLower(os.Getenv("GENEVA_LOGS_INTEGRATION")))
-	if containerType != "" && strings.Compare(strings.ToLower(containerType), "prometheussidecar") == 0 {
-		fs.sockAddress = fmt.Sprintf("/var/run/mdsd-%s/default_fluent.socket", containerType)
-	} else if genevaLogsIntegrationEnabled != "" && strings.Compare(strings.ToLower(genevaLogsIntegrationEnabled), "true") == 0 {
-		fs.sockAddress = "/var/run/mdsd-PrometheusSidecar/default_fluent.socket"
+	if (containerType != "" && strings.Compare(strings.ToLower(containerType), "prometheussidecar") == 0) ||
+		(genevaLogsIntegrationEnabled != "" && strings.Compare(strings.ToLower(genevaLogsIntegrationEnabled), "true") == 0) {
+		fs.sockAddress = FluentSocketAddressPrometheysSidecar
 	}
 	responseBytes, err := FluentSocketWriter.writeAndRead(fs, data)
 	defer FluentSocketWriter.disconnect(fs)
-	logger.Printf("Info::mdsd::Making call to FluentSocket: %s to write and read the config data", fs.sockAddress)
 	if err != nil {
 		logger.Printf("Error::mdsd::Failed to write and read the config data. Error message: %s", string(err.Error()))
 		return nil, err
 	}
-	logger.Printf("extensionconfig::getExtensionConfigResponse:: getting extension config from fluent socket-end")
-
 	return responseBytes, nil
 }
