6414d76f45d1a16fa64dfb42675879ae3baadc85
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-03-23 22:31:36 -0700

    fix bug in logs and events profile

--- build/linux/installer/scripts/dcr-config-parser.rb
+++ build/linux/installer/scripts/dcr-config-parser.rb
@@ -1,30 +1,30 @@
 #!/usr/local/bin/ruby
 
-require 'fileutils'
-require 'json'
-require_relative 'ConfigParseErrorLogger'
+require "fileutils"
+require "json"
+require_relative "ConfigParseErrorLogger"
 
-@os_type = ENV['OS_TYPE']
-@controllerType = ENV['CONTROLLER_TYPE']
-@containerType = ENV['CONTAINER_TYPE']
+@os_type = ENV["OS_TYPE"]
+@controllerType = ENV["CONTROLLER_TYPE"]
+@containerType = ENV["CONTAINER_TYPE"]
 @logs_and_events_streams = {
-  'CONTAINER_LOG_BLOB' => true,
-  'CONTAINERINSIGHTS_CONTAINERLOGV2' => true,
-  'KUBE_EVENTS_BLOB' => true,
-  'KUBE_POD_INVENTORY_BLOB' => true
+  "CONTAINER_LOG_BLOB" => true,
+  "CONTAINERINSIGHTS_CONTAINERLOGV2" => true,
+  "KUBE_EVENTS_BLOB" => true,
+  "KUBE_POD_INVENTORY_BLOB" => true,
 }
 @logs_and_events_only = false
 
-return if ENV['GENEVA_LOGS_INTEGRATION']&.strip&.casecmp?('true')
-return if !@os_type.nil? && !@os_type.empty? && @os_type.strip.casecmp('windows').zero?
-return unless ENV['USING_AAD_MSI_AUTH']&.strip&.casecmp?('true')
+return if ENV["GENEVA_LOGS_INTEGRATION"]&.strip&.casecmp?("true")
+return if !@os_type.nil? && !@os_type.empty? && @os_type.strip.casecmp("windows").zero?
+return unless ENV["USING_AAD_MSI_AUTH"]&.strip&.casecmp?("true")
 
-if !@controllerType.nil? && !@controllerType.empty? && @controllerType.strip.casecmp('daemonset').zero? \
+if !@controllerType.nil? && !@controllerType.empty? && @controllerType.strip.casecmp("daemonset").zero? \
   && @containerType.nil?
   begin
     file_path = nil
-    if Dir.exist?('/etc/mdsd.d/config-cache/configchunks')
-      Dir.glob('/etc/mdsd.d/config-cache/configchunks/*.json').each do |file|
+    if Dir.exist?("/etc/mdsd.d/config-cache/configchunks")
+      Dir.glob("/etc/mdsd.d/config-cache/configchunks/*.json").each do |file|
         if File.foreach(file).grep(/ContainerInsightsExtension/).any?
           file_path = file
           break # Exit the loop once a matching file is found
@@ -33,28 +33,29 @@ if !@controllerType.nil? && !@controllerType.empty? && @controllerType.strip.cas
     end
 
     # Raise an error if no JSON file is found
-    raise 'No JSON file found in the specified directory' unless file_path
+    raise "No JSON file found in the specified directory" unless file_path
 
     file_contents = File.read(file_path)
     data = JSON.parse(file_contents)
 
-    raise 'Invalid JSON structure: Missing required keys' unless data.is_a?(Hash) && data.key?('dataSources')
+    raise "Invalid JSON structure: Missing required keys" unless data.is_a?(Hash) && data.key?("dataSources")
 
     # Extract the stream values
-    streams = data['dataSources'].select { |ds| ds['id'] == 'ContainerInsightsExtension' }
-                                 .flat_map { |ds| ds['streams'] if ds.key?('streams') }
+    # If the DCR has GIG and ODS streams,then id will be either in the format ContainerInsightsExtension:ods-* or ContainerInsightsExtension:gigl-dce-*
+    streams = data["dataSources"].select { |ds| ds["id"].start_with?("ContainerInsightsExtension") }
+                                 .flat_map { |ds| ds["streams"] if ds.key?("streams") }
                                  .compact
-                                 .map { |stream| stream['stream'] if stream.key?('stream') }
+                                 .map { |stream| stream["stream"] if stream.key?("stream") }
                                  .compact
 
     # Check if there is a stream which is not part of the logs and events streams
     extra_stream = streams.any? { |stream| !@logs_and_events_streams.include?(stream) }
     unless extra_stream
       # Write the settings to file, so that they can be set as environment variables
-      puts 'DCR config matches Log and Events only profile. Setting LOGS_AND_EVENTS_ONLY to true'
+      puts "DCR config matches Log and Events only profile. Setting LOGS_AND_EVENTS_ONLY to true"
       @logs_and_events_only = true
     end
-    file = File.open('/opt/dcr_env_var', 'w')
+    file = File.open("/opt/dcr_env_var", "w")
     file.write("LOGS_AND_EVENTS_ONLY=#{@logs_and_events_only}\n")
     file.close
   rescue Exception => e
