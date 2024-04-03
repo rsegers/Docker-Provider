132906e69934f5183bc2a3eff6bd664351a45f06
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-04-03 11:02:26 -0700

    update windows ama build

--- kubernetes/windows/setup.ps1
+++ kubernetes/windows/setup.ps1
@@ -74,7 +74,8 @@ Write-Host ('Finished Extracting Certificate Generator Package')
 $windowsazuremonitoragent = [System.Environment]::GetEnvironmentVariable("WINDOWS_AMA_URL_NEW", "process")
 if ([string]::IsNullOrEmpty($windowsazuremonitoragent)) {
     Write-Host ('Environment variable WINDOWS_AMA_URL is not set. Using default value')
-    $windowsazuremonitoragent = "https://github.com/microsoft/Docker-Provider/releases/download/windows-ama-bits/GenevaMonitoringAgent.46.9.43.zip"
+    # TODO - Please update with official build which has GIG LA changes for Windows
+    $windowsazuremonitoragent = "https://github.com/microsoft/Docker-Provider/releases/download/mdsd-1.31.0/GenevaMonitoringAgent.46.16.58.zip"
 }
 Write-Host ('Installing Windows Azure Monitor Agent: ' + $windowsazuremonitoragent)
 try {
