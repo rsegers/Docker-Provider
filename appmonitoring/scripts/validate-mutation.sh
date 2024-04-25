c295b430270bf3769ceb78da5fec6ea512b32233
Ramanuj Bhattacharjee (from Dev Box) <rambhatt@microsoft.com>
2024-04-24 23:07:07 -0700

    fix mutation validation

--- appmonitoring/scripts/validate-mutation.sh
+++ appmonitoring/scripts/validate-mutation.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
+DEPLOYMENT_deployment#!/bin/bash
 
 # Define the pod name and namespace
-POD_DOTNET_NAME=$1
-POD_JAVA_NAME=$2
-POD_NODEJS_NAME=$3
+DEPLOYMENT_DOTNET_NAME=$1
+DEPLOYMENT_JAVA_NAME=$2
+DEPLOYMENT_NODEJS_NAME=$3
 NAMESPACE=$4
 
 # Define the property to check for
 PROPERTY="APPLICATIONINSIGHTS_CONNECTION_STRING"
 
-DOTNET_POD_NAME=$(kubectl get pods -n "$NAMESPACE" -o custom-columns=NAME:.metadata.name | grep "$POD_DOTNET_NAME")
-JAVA_POD_NAME=$(kubectl get pods -n "$NAMESPACE" -o custom-columns=NAME:.metadata.name | grep "$POD_JAVA_NAME")
-NODEJS_POD_NAME=$(kubectl get pods -n "$NAMESPACE" -o custom-columns=NAME:.metadata.name | grep "$POD_NODEJS_NAME")
+DOTNET_DEPLOYMENT_NAME=$(kubectl get deployment -n "$NAMESPACE" -o custom-columns=NAME:.metadata.name | grep "$DEPLOYMENT_DOTNET_NAME")
+JAVA_DEPLOYMENT_NAME=$(kubectl get deployment -n "$NAMESPACE" -o custom-columns=NAME:.metadata.name | grep "$DEPLOYMENT_JAVA_NAME")
+NODEJS_DEPLOYMENT_NAME=$(kubectl get deployment -n "$NAMESPACE" -o custom-columns=NAME:.metadata.name | grep "$DEPLOYMENT_NODEJS_NAME")
 
 checkit() {
     local podName="$1"  # The first argument to the function is stored in 'name'
-    POD_YAML=$(kubectl get pod "$podName" -n "$NAMESPACE" -o yaml)
+    DEPLOYMENT_YAML=$(kubectl get pod "$podName" -n "$NAMESPACE" -o yaml)
 
     # Check for the property
-    if echo "$POD_YAML" | grep -q "$PROPERTY"; then
+    if echo "$DEPLOYMENT_YAML" | grep -q "$PROPERTY"; then
         echo "Property $PROPERTY found in pod $podName"
         # You can add additional commands here to process the property
     else
@@ -26,6 +26,6 @@ checkit() {
     fi
 }
 
-checkit "$DOTNET_POD_NAME" 
-checkit "$JAVA_POD_NAME" 
-checkit "$NODEJS_POD_NAME" 
+checkit "$DOTNET_DEPLOYMENT_NAME" 
+checkit "$JAVA_DEPLOYMENT_NAME" 
+checkit "$NODEJS_DEPLOYMENT_NAME" 
