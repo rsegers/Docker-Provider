b3cb702e9872ad4fbaa85a6aa165d8b91c3537ea
Janvi Jatakia (from Dev Box) <jajataki@microsoft.com>
2024-04-30 14:54:23 -0700

    update ai instrumentation key

--- kubernetes/windows/main.ps1
+++ kubernetes/windows/main.ps1
@@ -93,42 +93,6 @@ function Set-AirgapCloudSpecificApplicationInsightsConfig {
          Write-Host "Successfully set environment variable APPLICATIONINSIGHTS_ENDPOINT - $($appInsightsEndpoint) for target 'machine'..."
      }
 
-     # Check if the instrumentation key needs to be fetched from a storage account (as in airgapped clouds)
-     $aiKeyURl = [System.Environment]::GetEnvironmentVariable('APPLICATIONINSIGHTS_AUTH_URL')
-     if ($aiKeyURl) {
-         $aiKeyFetched = ""
-         # retry up to 5 times
-         for ( $i = 1; $i -le 4; $i++) {
-             try {
-                 $response = Invoke-WebRequest -uri $aiKeyURl -UseBasicParsing -TimeoutSec 5 -ErrorAction:Stop
-
-                 if ($response.StatusCode -ne 200) {
-                     Write-Host "Expecting reponse code 200, was: $($response.StatusCode), retrying"
-                     Start-Sleep -Seconds ([MATH]::Pow(2, $i) / 4)
-                 }
-                 else {
-                     $aiKeyFetched = $response.Content
-                     break
-                 }
-             }
-             catch {
-                 Write-Host "Exception encountered fetching instrumentation key:"
-                 Write-Host $_.Exception
-             }
-         }
-
-         # Check if the fetched IKey was properly encoded. if not then turn off telemetry
-         if ($aiKeyFetched -match '^[A-Za-z0-9=]+$') {
-             Write-Host "Using cloud-specific instrumentation key"
-             Set-ProcessAndMachineEnvVariables "APPLICATIONINSIGHTS_AUTH" $aiKeyFetched
-         }
-         else {
-             # Couldn't fetch the Ikey, turn telemetry off
-             Write-Host "Could not get cloud-specific instrumentation key (network error?). Disabling telemetry"
-             Set-ProcessAndMachineEnvVariables "DISABLE_TELEMETRY" "True"
-         }
-     }
-
      $aiKeyDecoded = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($env:APPLICATIONINSIGHTS_AUTH))
      Set-ProcessAndMachineEnvVariables "TELEMETRY_APPLICATIONINSIGHTS_KEY" $aiKeyDecoded
 }
@@ -151,10 +115,16 @@ function Set-CloudSpecificApplicationInsightsConfig {
         }
         "usnat" {
             Write-Host "Set-CloudSpecificApplicationInsightsConfig: Setting Application Insights configuration for USNat Cloud"
+            Set-ProcessAndMachineEnvVariables "APPLICATIONINSIGHTS_AUTH" "YTk5NTlkNDYtYzE3Zi0xZDYxLWJhODgtZWU3NDFjMGI3MTliCg=="
+            # IngestionEndpoint: usnateast-0.in.applicationinsights.azure.eaglex.ic.gov
+            Set-ProcessAndMachineEnvVariables "APPLICATIONINSIGHTS_ENDPOINT" "https://dc.applicationinsights.azure.eaglex.ic.gov/v2/track"
             Set-AirgapCloudSpecificApplicationInsightsConfig
         }
         "ussec" {
             Write-Host "Set-CloudSpecificApplicationInsightsConfig: Setting Application Insights configuration for USSec Cloud"
+            Set-ProcessAndMachineEnvVariables "APPLICATIONINSIGHTS_AUTH" "NTc5ZDRiZjUtMTA1Mi0wODQzLThhNTYtMjU5YzEyZmJhZTkyCg=="
+              # IngestionEndpoint: usseceast-0.in.applicationinsights.azure.microsoft.scloud
+            Set-ProcessAndMachineEnvVariables "APPLICATIONINSIGHTS_ENDPOINT" "https://dc.applicationinsights.azure.microsoft.scloud/v2/track"
             Set-AirgapCloudSpecificApplicationInsightsConfig
         }
         default {
