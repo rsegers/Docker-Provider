7a2de6aefe4447489acbfd9d1b46414bb0d5a4fa
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-05-02 11:57:33 -0700

    mdsd version update

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -40,12 +40,12 @@ gem uninstall uri --version 0.11.0
 if [ "${ARCH}" != "arm64" ]; then
     # mdsd version with 50k eps changes for x64
     # TODO - For PROD release, make sure to consume the MDSD version from Mariner package repo
-    wget "https://github.com/microsoft/Docker-Provider/releases/download/mdsd-1.31.0/azure-mdsd-1.31.0-build.master.2953.x86_64.rpm" -O azure-mdsd.rpm
-    sudo tdnf install -y azure-mdsd.rpm
-else
-    sudo tdnf install -y azure-mdsd-1.30.3
-fi
+    wget "https://github.com/microsoft/Docker-Provider/releases/download/mdsd-1.31.0/azure-mdsd-1.31.1-build.master.500.x86_64.rpm" -O azure-mdsd.rpm
 
+else
+    wget "https://github.com/microsoft/Docker-Provider/releases/download/mdsd-1.31.0/azure-mdsd-1.31.1-build.master.500.aarch64.rpm" -O azure-mdsd.rpm
+fi
+sudo tdnf install -y azure-mdsd.rpm
 # validate latest version of MDSD and see if this works in GIG mode
 # sudo tdnf install -y azure-mdsd-1.30.3
 cp -f $TMPDIR/mdsd.xml /etc/mdsd.d
