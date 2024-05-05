2ffc627920dcec005666e01a07f7a05da568c878
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-05-04 23:04:15 -0700

    increase storage limit size to 2GB

--- build/common/installer/scripts/fluent-bit-conf-customizer.rb
+++ build/common/installer/scripts/fluent-bit-conf-customizer.rb
@@ -28,7 +28,7 @@ end
 @default_high_log_scale_service_interval = "1"
 @default_high_log_scale_max_storage_chunks_up = "500" # Each chunk size is ~2MB
 @default_high_log_scale_max_storage_type = "filesystem" # filesystem = memory + filesystem in fluent-bit
-@default_high_log_scale_max_storage_total_limit_size = "1000M"
+@default_high_log_scale_max_storage_total_limit_size = "2G"
 
 def is_number?(value)
   true if Integer(value) rescue false
@@ -62,7 +62,7 @@ end
 
 def substituteStorageTotalLimitSize(new_contents)
    if is_high_log_scale_mode?
-      new_contents = new_contents.gsub("#${OMS_STORAGE_TOTAL_LIMIT_SIZE_MB}", "storage.total_limit_size " + @default_high_log_scale_max_storage_total_limit_size)
+      new_contents = new_contents.gsub("#${OMS_STORAGE_TOTAL_LIMIT_SIZE_MB}", "storage.total_limit_size  " + @default_high_log_scale_max_storage_total_limit_size)
    else
       new_contents = new_contents.gsub("\n    #${OMS_STORAGE_TOTAL_LIMIT_SIZE_MB}\n", "\n")
    end
