2eaab8970281340416a72ba6ce7e103370b75c9e
longwan <longwan@microsoft.com>
2024-05-23 23:28:04 +0000

    update unit tests with more functional tests and empty tests

--- source/plugins/go/src/oms_test.go
+++ source/plugins/go/src/oms_test.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"testing"
-	"github.com/stretchr/testify/assert"
 	"fmt"
+	"reflect"
+	"github.com/stretchr/testify/assert"
 )
 
 var kubernetesJSON = `{
@@ -25,7 +26,7 @@ var kubernetesJSON = `{
 	"host":"test-agentpool-test-test000001",
 	"container_name":"test-publisher",
 	"docker_id":"test1234567890123213213123213213213213",
-	"container_hash":publisher@sha256:test1234567890123213213123213213213213",
+	"container_hash":"publisher@sha256:test1234567890123213213123213213213213",
 	"container_image":"test-publisher:1.0.67"
 }`
 
@@ -62,4 +63,159 @@ func TestPostDataHelperKuberneteMetadata(t *testing.T) {
 	output := PostDataHelper([]map[interface{}]interface{}{record})
 
 	assert.Greater(t, output, 0, "Expected output to be greater than 0 indicating processing occurred")
+}
+
+// Test PostDataHelper with empty tailPluginRecords
+func TestPostDataHelperEmpty(t *testing.T) {
+	tailPluginRecords := []map[interface{}]interface{}{}
+	expectedOutput := 1
+	output := PostDataHelper(tailPluginRecords)
+	if output != expectedOutput {
+		t.Errorf("Expected output to be %d, but got %d", expectedOutput, output)
+	}
+}
+
+// Test PostDataHelper with multiple tailPluginRecords
+func TestPostDataHelperMultiple(t *testing.T) {
+	tailPluginRecords := []map[interface{}]interface{}{
+		{
+			"filepath": "/var/log/containers/pod_xyz.log",
+			"stream":   "stdout",
+			"kubernetes": map[interface{}]interface{}{
+				"pod_name":        "test-publisher-ds-bssg6",
+				"namespace_name":  "kube-system",
+				"pod_id":          "93bf47d2-5c1a-42bc-test-481939a93a66",
+				"labels": map[interface{}]interface{}{
+					"app":                          "test",
+					"controller-revision-hash":     "f48799794",
+					"dsName":                       "defender-publisher-ds",
+					"kubernetes.azure.com/managedby": "aks",
+					"pod-template-generation":       "2",
+				},
+				"annotations": map[interface{}]interface{}{
+					"kubernetes.io/config.seen":   "2023-10-02T08:21:49.954540360Z",
+					"kubernetes.io/config.source": "api",
+				},
+				"host":             "test-agentpool-test-test000001",
+				"container_name":   "test-publisher",
+				"docker_id":        "test1234567890123213213123213213213213",
+				"container_hash":   "publisher@sha256:test1234567890123213213123213213213213",
+				"container_image":  "test-publisher:1.0.67",
+			},
+		},
+		{
+			"filepath": "/var/log/containers/pod_abc.log",
+			"stream":   "stderr",
+			"kubernetes": map[interface{}]interface{}{
+				"pod_name":        "test-consumer-ds-abcde",
+				"namespace_name":  "default",
+				"pod_id":          "a1b2c3d4e5f6",
+				"labels": map[interface{}]interface{}{
+					"app":                          "test",
+					"controller-revision-hash":     "f48799794",
+					"dsName":                       "defender-consumer-ds",
+					"kubernetes.azure.com/managedby": "aks",
+					"pod-template-generation":       "1",
+				},
+				"annotations": map[interface{}]interface{}{
+					"kubernetes.io/config.seen":   "2023-10-02T08:21:49.954540360Z",
+					"kubernetes.io/config.source": "api",
+				},
+				"host":             "test-agentpool-test-test000002",
+				"container_name":   "test-consumer",
+				"docker_id":        "abcde12345",
+				"container_hash":   "consumer@sha256:abcde12345",
+				"container_image":  "test-consumer:2.0.12",
+			},
+		},
+	}
+	expectedOutput := 2
+	output := PostDataHelper(tailPluginRecords)
+	if output != expectedOutput {
+		t.Errorf("Expected output to be %d, but got %d", expectedOutput, output)
+	}
+}
+
+func TestConvertKubernetesMetadata(t *testing.T) {
+	kubernetesMetadataJson := map[interface{}]interface{}{
+		"pod_name":       "test-pod",
+		"namespace_name": "test-namespace",
+		"labels": map[interface{}]interface{}{
+			"app": "test-app",
+		},
+		"annotations": map[interface{}]interface{}{
+			"annotation_key": "annotation_value",
+		},
+	}
+
+	expectedResult := map[string]interface{}{
+		"pod_name":       "test-pod",
+		"namespace_name": "test-namespace",
+		"labels": map[string]interface{}{
+			"app": "test-app",
+		},
+		"annotations": map[string]interface{}{
+			"annotation_key": "annotation_value",
+		},
+	}
+
+	result, err := convertKubernetesMetadata(kubernetesMetadataJson)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Expected result to be %v, but got %v", expectedResult, result)
+	}
+}
+
+func TestProcessIncludes(t *testing.T) {
+	kubernetesMetadataMap := map[string]interface{}{
+		"pod_name":"test-publisher-ds-bssg6",
+		"namespace_name":"kube-system",
+		"pod_id":"93bf47d2-5c1a-42bc-test-481939a93a66",
+		"labels": map[string]interface{}{
+			"app":"test",
+			"controller-revision-hash":"f48799794",
+			"dsName":"defender-publisher-ds",
+			"kubernetes.azure.com/managedby":"aks",
+			"pod-template-generation":"2",
+		},
+		"annotations": map[string]interface{}{
+			"test":"2023-10-02T08:21:49.954540360Z",
+		},
+		"host":"test-agentpool-test-test000001",
+		"container_name":"test-publisher",
+		"docker_id":"test1234567890123213213123213213213213",
+		"container_hash":"publisher@sha256:test1234567890123213213123213213213213",
+		"container_image":"docker.io/test-publisher:1.0.67",
+	}
+
+	includesList := []string{
+		"poduid", "podlabels", "podannotations", "imageid", "imagerepo", //"imagetag", //"image",
+	}
+
+	expectedResult := map[string]interface{}{
+		//"image": "test-publisher",
+		"imageID": "sha256:test1234567890123213213123213213213213",
+		"imageRepo": "docker.io",
+		//"imageTag": "1.0.67",
+		"podAnnotations": map[string]interface{}{
+			"test": "2023-10-02T08:21:49.954540360Z",
+		},
+		"podLabels": map[string]interface{}{
+			"app": "test",
+			"controller-revision-hash": "f48799794",
+			"dsName": "defender-publisher-ds",
+			"kubernetes.azure.com/managedby": "aks",
+			"pod-template-generation": "2",
+		},
+		"podUid": "93bf47d2-5c1a-42bc-test-481939a93a66",
+	}
+
+	result := processIncludes(kubernetesMetadataMap, includesList)
+
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Expected result to be %v, but got %v", expectedResult, result)
+	}
 }
