9315bb90a439809a98d3c436795be971b8e13900
Ramanuj Bhattacharjee <103154538+ramanujbhattacharjee93@users.noreply.github.com>
2024-07-11 15:31:32 -0700

    Fix build Pipeline for Appmonitoring (#1291)
    * add changes
    
    * add changes
    
    * add changes

--- appmonitoring/scripts/validate-aks-charts.sh
+++ appmonitoring/scripts/validate-aks-charts.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-az account set --subscription 66010356-d8a5-42d3-8593-6aaa3aeb1c11
-az aks get-credentials --resource-group appmonitoring-pipeline-test --name aks-pipeline-testbed-ai
-
+az account set --subscription 5a3b3ba4-3a42-42ae-b2cb-f882345803bc
+az aks get-credentials --resource-group aks-appmonitoring-pipeline --name appmonitoring-webhook-testbed
 
 DEPLOYMENT=app-monitoring-webhook-deployment
 SECRET_STORE=app-monitoring-webhook-cert
